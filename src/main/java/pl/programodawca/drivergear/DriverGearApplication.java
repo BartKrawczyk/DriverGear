3741c169007a38e74eba9e10c32f7c7f34afc2fa
Bart Krawczyk <bartek.krawczyk@interia.pl>
2023-02-19 17:53:39 +0100

    Initial commit

---
+++ src/main/java/pl/programodawca/drivergear/DriverGearApplication.java
@@ -0,0 +1,13 @@
+package pl.programodawca.drivergear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverGearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverGearApplication.class, args);
+    }
+
+}
