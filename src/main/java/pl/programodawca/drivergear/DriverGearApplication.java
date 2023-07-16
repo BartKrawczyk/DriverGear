814b5af5dfa360341e6f0d496971dcb91428efab
Bart Krawczyk <bartek.krawczyk@interia.pl>
2023-07-16 14:14:14 +0200

    large changes in versions of spiring boot modules

--- src/main/java/pl/programodawca/drivergear/DriverGearApplication.java
+++ src/main/java/pl/programodawca/drivergear/DriverGearApplication.java
@@ -1,6 +1,5 @@
 package pl.programodawca.drivergear;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- src/main/java/pl/programodawca/drivergear/config/SecurityConfig.java
+++ src/main/java/pl/programodawca/drivergear/config/SecurityConfig.java
@@ -9,11 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import pl.programodawca.drivergear.service.UserDetailServiceImpl;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final UserDetailServiceImpl userDetailsService;
 
@@ -32,25 +35,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/").permitAll()
+                // disabling csrf since we won't use form login
+                .csrf().disable()
+                // giving permission to every request for /login endpoint
+                .authorizeRequests().antMatchers("/login").permitAll()
+                // for everything else, the user has to be authenticated
                 .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/dashboard")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .permitAll()
-                .and()
-                .csrf().disable(); // Wyłączamy CSRF; w rzeczywistym środowisku zaleca się pozostawić włączone.
+                // setting stateless session, because we choose to implement Rest API
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // adding the custom filter before UsernamePasswordAuthenticationFilter in the filter chain
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/admin/**").hasRole("ADMIN")
+//                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/dashboard")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login?logout")
+//                .permitAll()
+//                .and()
+//                .csrf().disable(); // Wyłączamy CSRF; w rzeczywistym środowisku zaleca się pozostawić włączone.
+//    }
 }
